99fdfab131cb258a46a3b2766cc564c5b466e0ce
Myungin Lee <benedic.lee@gmail.com>
2024-02-27 12:19:23 -0500

    update

--- assignment4_template/Assets/camera_move.cs
+++ assignment4_template/Assets/camera_move.cs
@@ -10,7 +10,7 @@ public class camera_move : MonoBehaviour
     //This is the second Camera and is assigned in inspector
     public float speed = 5.0f;
     public float sensitivity = 5.0f;
-
+    public float test = 0f;
     void Start()
     {
         //This gets the Main Camera from the Scene
@@ -24,7 +24,7 @@ public class camera_move : MonoBehaviour
         // Move the camera forward, backward, left, and right
         m_MainCamera.transform.position += m_MainCamera.transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
         m_MainCamera.transform.position += m_MainCamera.transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
+        Debug.Log(m_MainCamera.transform.position);
         // Rotate the camera based on the mouse movement
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
